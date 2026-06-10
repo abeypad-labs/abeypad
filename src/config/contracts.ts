912988b5e90f50539f7436154b8a6cd265337acf
sudotx <agbadilawal78@gmail.com>
2026-06-10 22:53:52 +0100

    update CAs

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -6,7 +6,7 @@ export const CONTRACT_ADDRESSES = {
     tokenFactory: "0x4b840A32914f48b8997970fB14f72E0E926bf87D" as Address,
     presaleFactory: "0x88C4973Dc2C9A1092525De1b627A3A14302dF6e7" as Address,
     nftFactory: "0x0274619238c6C9d136e37b095f50DF47F79a9Da0" as Address,
-    registry: "0x90fCa1a109159f6AF312567dD253FfE8a9173f8e" as Address,
-    resolver: "0x59f78fe1955e228fD8A9Abc1FC38aC2e89E3d963" as Address,
-    registrar: "0x13b0e0fdAc877EB7822ca462231874425432D197" as Address, // domain 
+    registry: "0x47b0fFb72934c9b97A66d39668b29dA373a49117" as Address,
+    resolver: "0xb86c03f93d0Ab5542fb57CD8D04068E60cd086c4" as Address,
+    registrar: "0x68060d7182240f37779D6f7E7E963988b79D8C14" as Address,
 };
