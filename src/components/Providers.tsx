129df6312f98e91543b868404d503ac4bfe67b77
sudotx <agbadilawal78@gmail.com>
2026-06-11 22:10:00 +0100

    feat: new adds

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,17 +1,17 @@
-import { abeychainDevnet } from "@/config";
 import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 import { useState } from "react";
+import { abey } from "viem/chains";
 import { WagmiProvider, http } from "wagmi";
 
 const config = getDefaultConfig({
   appName: 'AbeyPad',
   projectId: '9ef8a1835f8d9515949514f77259f972',
-  chains: [abeychainDevnet],
+  chains: [abey],
   transports: {
-    [abeychainDevnet.id]: http(),
+    [abey.id]: http(),
   },
 });
 
